ba67c84cb9cbe4c97a2f43009881ec204940f7a4
JavierDeDiegoGuzman <j.dediegoguzman@gmail.com>
2024-12-28 11:12:42 +0100

    fix logout button

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -8,8 +8,9 @@ export default async function Dashboard() {
   return (
     <main className="min-h-screen p-8 pb-24">
       <section className="max-w-xl mx-auto space-y-8">
-        <div className="flex justify-between items-center">
-          <h1 className="text-3xl md:text-4xl font-extrabold">Mis Barajas</h1>
+        <div className="flex items-center justify-between gap-4">
+          <ButtonAccount />
+          <h1 className="text-3xl md:text-4xl font-extrabold whitespace-nowrap">Mis Barajas</h1>
           <CreateDeckButton />
         </div>
         <DeckList />
